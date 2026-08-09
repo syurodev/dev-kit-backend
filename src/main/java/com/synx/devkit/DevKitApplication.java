9fe8fb165b5c5a090ff7f7fae2a99168813a63f4
Syuro <64142347+syurodev@users.noreply.github.com>
2026-08-09 11:46:53 +0700

    init

---
+++ src/main/java/com/synx/devkit/DevKitApplication.java
@@ -0,0 +1,13 @@
+package com.synx.devkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevKitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevKitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/synx/devkit/DevKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synx.devkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevKitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
